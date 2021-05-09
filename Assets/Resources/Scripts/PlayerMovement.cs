4b8468a0bacd907f34f4aef619d9ce870ce3ac02
Shamim Akhtar <shamim.akhtar@gmail.com>
2021-05-09 17:03:24 +0800

    Added ThirdPersonCamera

--- Assets/Resources/Scripts/PlayerMovement.cs
+++ Assets/Resources/Scripts/PlayerMovement.cs
@@ -9,10 +9,14 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController mCharacterController;
     public Animator mAnimator;
 
-    public float mWalkSpeed = 2f;
-    public float mRunSpeed = 4.0f;
+    public float mWalkSpeed = 1.5f;
+    public float mRunSpeed = 3.0f;
     public float mRotationSpeed = 50.0f;
     public float mGravity = -30.0f;
+
+    [Tooltip("Only useful with Follow and Independent Rotation - third - person camera control")]
+    public bool mFollowCameraForward = false;
+    public float mTurnRate = 200.0f;
     private Vector3 mVelocity = new Vector3(0.0f, 0.0f, 0.0f);
 
     // Start is called before the first frame update
@@ -37,8 +41,25 @@ public class PlayerMovement : MonoBehaviour
             speed = mRunSpeed;
         }
 
+        if (mFollowCameraForward)
+        {
+            // Only allow aligning of player's direction when there is a movement.
+            //if (v > 0.1 || v < -0.1 || h > 0.1 || h < -0.1)
+            {
+                // rotate player towards the camera forward.
+                Vector3 eu = Camera.main.transform.rotation.eulerAngles;
+                transform.rotation = Quaternion.RotateTowards(
+                    transform.rotation,
+                    Quaternion.Euler(0.0f, eu.y, 0.0f),
+                    mTurnRate * Time.deltaTime);
+            }
+        }
+        else
+        {
+            transform.Rotate(0.0f, h * mRotationSpeed * Time.deltaTime, 0.0f);
+        }
+        //transform.Rotate(0.0f, h * mRotationSpeed * Time.deltaTime, 0.0f);
         mCharacterController.Move(transform.forward * v * speed * Time.deltaTime);
-        transform.Rotate(0.0f, h * mRotationSpeed * Time.deltaTime, 0.0f);
         if (mAnimator != null)
         {
             mAnimator.SetFloat("Speed", v * speed / mRunSpeed);
